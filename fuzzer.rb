9b3bfc2619ebc5038b85bfb796c78d1eac5fb7c0
vertroa <vertroa@gmail.com>
2015-08-22 15:29:06 -0400

    Implemented Faraday HTTP connection

--- fuzzer.rb
+++ fuzzer.rb
@@ -3,6 +3,7 @@
 require 'slop'
 require 'cgi'
 require 'uri'
+require 'faraday'
 
 opts = Slop.parse do |o|
 	o.bool '-g', '--get', 'If you are fuzzing a GET request'
@@ -17,14 +18,23 @@ opts = Slop.parse do |o|
 	end
 end
 
-## Begin the fuzzing
-def fuzzer(url, file, params, request, *cookie) 
-	params.each do |x, y| 
-		puts  "#{x} = #{y}"
+## Method that places the fuzzing requests 
+def fuzzer(url, payloads, params, request, *cookie) 
+
+	conn_options = Hash.new
+		conn_options[:proxy] = 'http://127.0.0.1:8080'
+
+	conn = Faraday.new(url, conn_options)
+
+	conn.post do |req|
+		req.url '/WebGoat/start.mvc'
+		req.headers['Cookie'] = cookie 
 	end
 
+	payloads.each do |payload|
 
-	
+
+	end
 end
 
 ## Initialize variables
